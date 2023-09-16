68913910e89da65b14883caba239abbfedf16090
SovikPC <vital.gurov2017@gmail.com>
2023-09-16 09:17:30 +0800

    Create Spring project

---
+++ src/test/java/ru/gurov/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gurov.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
